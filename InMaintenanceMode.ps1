b01afc80948190fa830de206e28e6dc7ba6bd085
snehonandy <72209652+snehonandy@users.noreply.github.com>
2020-10-05 16:31:30 +0530

    Add files via upload
    Check if VDI is in Maintenance Mode 
    Move VDI to Maintenance Mode

---
+++ InMaintenanceMode.ps1
@@ -0,0 +1,27 @@
+﻿[CmdletBinding()]
+#Parameter Declaration
+Param(
+  [Parameter(Mandatory=$True)]
+   [string]$UserName,
+ 
+   [Parameter(Mandatory=$True)]
+   [string]$CitrixCatalogName
+)
+
+
+$controller = ""
+$remotesession = New-PSSession -ComputerName $controller
+Invoke-Command -Command {Add-PSSnapin Citrix.*} -Session $remotesession     
+Import-PSSession -Session $remotesession -Module Citrix.* -Prefix RM -AllowClobber
+$UserName="PBI\$UserName"
+
+$VDI = Get-RMBrokerDesktop -CatalogName $CitrixCatalogName -AssociatedUserName $UserName
+
+$MachineInMaintenanceMode = $VDI.InMaintenanceMode
+$machineName=$VDI.MachineName
+
+Write-Host "InMaintenanceMode="$MachineInMaintenanceMode 
+
+
+
+
---
+++ MoveToMaintenanceMode.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]
+#Parameter Declaration
+Param(
+  [Parameter(Mandatory=$True)]
+   [string]$VDIName
+)
+
+
+$controller = ""
+$remotesession = New-PSSession -ComputerName $controller
+Invoke-Command -Command {Add-PSSnapin Citrix.*} -Session $remotesession     
+Import-PSSession -Session $remotesession -Module Citrix.* -Prefix RM -AllowClobber
+$error.Clear()
+New-RMBrokerHostingPowerAction -MachineName $VDIName -Action shutdown
+sleep -Seconds 120
+$VDI = Set-RMBrokerPrivateDesktop "$VDIName" -InMaintenanceMode $True
+
+
+Write-Host $VDI
+
+
+if($error.Count -eq '0')
+{
+    Write-Host "Success"
+}
+else
+{
+    Write-Host "Failed"
+}
+
+(Get-WmiObject Win32_Process -ComputerName $controller -ErrorAction SilentlyContinue | ?{ $_.ProcessName -match "wsmprovhost" }).Terminate()
