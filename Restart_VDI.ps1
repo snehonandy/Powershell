6b82a2ebce8685bea5dd8ceffe74dd78d569faea
snehonandy <72209652+snehonandy@users.noreply.github.com>
2020-10-05 16:32:25 +0530

    Add files via upload
    Restart a VDI

---
+++ Restart_VDI.ps1
@@ -0,0 +1,34 @@
+﻿
+[CmdletBinding()]
+#Parameter Declaration
+Param(
+  
+   [Parameter(Mandatory=$True)]
+   [string]$vdi
+ )
+
+
+$controller = ""
+$remotesession = New-PSSession -ComputerName $controller
+Invoke-Command -Command {Add-PSSnapin Citrix.*} -Session $remotesession     
+Import-PSSession -Session $remotesession -Module Citrix.* -Prefix RM -AllowClobber
+
+New-RMBrokerHostingPowerAction -MachineName $vdi -Action 'Reset'
+
+sleep -Seconds 900
+
+$machine_state = Get-RMBrokerMachine -MachineName $vdi
+
+$Power_State = $machine_state.PowerState
+
+if($Power_State -eq "On")
+{
+    Write-Host "True"
+}
+else
+{
+    Write-Host "False"
+}
+
+
+(Get-WmiObject Win32_Process -ComputerName $controller -ErrorAction SilentlyContinue | ?{ $_.ProcessName -match "wsmprovhost" }).Terminate()
